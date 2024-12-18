3429a6f5ab4bb92d0afb1a05c8e019f2126db636
taokz <taokz@xctctech.com>
2024-12-18 09:52:06 +0800

    init

---
+++ build/linux/x86_64/release/build/.deps/exercise10/linux/x86_64/release/10_trivial/main.cpp.o.d
@@ -0,0 +1,21 @@
+{
+    depfiles_gcc = "main.o: 10_trivial/main.cpp 10_trivial/../exercise.h\
+",
+    files = {
+        "10_trivial/main.cpp"
+    },
+    values = {
+        "/usr/bin/gcc",
+        {
+            "-m64",
+            "-fvisibility=hidden",
+            "-fvisibility-inlines-hidden",
+            "-Wall",
+            "-O3",
+            "-std=c++17",
+            "-finput-charset=UTF-8",
+            "-fexec-charset=UTF-8",
+            "-DNDEBUG"
+        }
+    }
+}
